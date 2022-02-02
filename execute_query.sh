2fb772bf2c8fe0ca1c907ef6705c7e305c240146
pivotal-bernard-royet <be.royet@gmail.com>
2022-02-02 14:29:20 +0100

    Minor changes

--- execute_query.sh
+++ execute_query.sh
@@ -6,18 +6,19 @@ else
   CONC=$1
 fi
 
+database=gpmart
 
-# Keep running queries (using a vmart query as an example) in concurrent vsql threads.
+# Keep running queries (using a gpmart query as an example) in concurrent vsql threads.
 for x in $(seq 1 "$CONC" )
   do 
-         psql -U vmart1 -f vmart_queries.sql -o vmart_queries.vmart1.log vmart &
-         psql -U vmart2 -f vmart_queries.sql -o vmart_queries.vmart2.log vmart &
-         psql -U vmart3 -f vmart_queries.sql -o vmart_queries.vmart3.log vmart &
-         psql -U vmart4 -f vmart_queries.sql -o vmart_queries.vmart4.log vmart &
-         psql -U vmart5 -f vmart_queries.sql -o vmart_queries.vmart5.log vmart &
-         psql -U vmart6 -f vmart_queries.sql -o vmart_queries.vmart6.log vmart &
-         psql -U vmart7 -f vmart_queries.sql -o vmart_queries.vmart7.log vmart &
-         psql -U vmart8 -f vmart_queries.sql -o vmart_queries.vmart8.log vmart &
-         psql -U vmart9 -f vmart_queries.sql -o vmart_queries.vmart9.log vmart &
-         psql -U vmart10 -f vmart_queries.sql -o vmart_queries.vmart10.log vmart
+         psql -U gpmart1 -f gpmart_queries.sql -o gpmart_queries.gpmart1.log $database &
+         psql -U gpmart2 -f gpmart_queries.sql -o gpmart_queries.gpmart2.log $database &
+         psql -U gpmart3 -f gpmart_queries.sql -o gpmart_queries.gpmart3.log $database &
+         psql -U gpmart4 -f gpmart_queries.sql -o gpmart_queries.gpmart4.log $database &
+         psql -U gpmart5 -f gpmart_queries.sql -o gpmart_queries.gpmart5.log $database &
+         psql -U gpmart6 -f gpmart_queries.sql -o gpmart_queries.gpmart6.log $database &
+         psql -U gpmart7 -f gpmart_queries.sql -o gpmart_queries.gpmart7.log $database &
+         psql -U gpmart8 -f gpmart_queries.sql -o gpmart_queries.gpmart8.log $database &
+         psql -U gpmart9 -f gpmart_queries.sql -o gpmart_queries.gpmart9.log $database &
+         psql -U gpmart10 -f gpmart_queries.sql -o gpmart_queries.gpmart10.log $database
   done
