63c37d0a72b878985fffffbce8ad0302b127b87f
pivotal-bernard-royet <be.royet@gmail.com>
2022-02-02 11:39:54 +0100

    Update gpmart_gpload_data.sh

--- gpmart_gpload_data.sh
+++ gpmart_gpload_data.sh
@@ -1,17 +1,18 @@
 export data_directory=$1
+export database=$2
 
-gpload -d vmart -f Call_Center_Dimension.gpload.yml
-gpload -d vmart -f Customer_Dimension.gpload.yml
-gpload -d vmart -f Date_Dimension.gpload.yml
-gpload -d vmart -f Date_Dimension.tbl.gpload.yml
-gpload -d vmart -f Employee_Dimension.gpload.yml
-gpload -d vmart -f Inventory_Fact.gpload.yml
-gpload -d vmart -f Online_Sales_Fact.gpload.yml
-gpload -d vmart -f Product_Dimension.gpload.yml
-gpload -d vmart -f Promotion_Dimension.gpload.yml
-gpload -d vmart -f Shipping_Dimension.gpload.yml
-gpload -d vmart -f Store_Dimension.gpload.yml
-gpload -d vmart -f Store_Orders_Fact.gpload.yml
-gpload -d vmart -f Store_Sales_Fact.gpload.yml
-gpload -d vmart -f Vendor_Dimension.gpload.yml
-gpload -d vmart -f Warehouse_Dimension.gpload.yml
+gpload -d $database -f Call_Center_Dimension.gpload.yml
+gpload -d $database -f Customer_Dimension.gpload.yml
+gpload -d $database -f Date_Dimension.gpload.yml
+gpload -d $database -f Date_Dimension.tbl.gpload.yml
+gpload -d $database -f Employee_Dimension.gpload.yml
+gpload -d $database -f Inventory_Fact.gpload.yml
+gpload -d $database -f Online_Sales_Fact.gpload.yml
+gpload -d $database -f Product_Dimension.gpload.yml
+gpload -d $database -f Promotion_Dimension.gpload.yml
+gpload -d $database -f Shipping_Dimension.gpload.yml
+gpload -d $database -f Store_Dimension.gpload.yml
+gpload -d $database -f Store_Orders_Fact.gpload.yml
+gpload -d $database -f Store_Sales_Fact.gpload.yml
+gpload -d $database -f Vendor_Dimension.gpload.yml
+gpload -d $database -f Warehouse_Dimension.gpload.yml
